3b119bcfbb0e1a9c89a9bbd4c7d6468139c73a09
Petra Husárová <>
2025-09-24 13:58:59 +0200

    add favicon, make basic mobile layout in index.html + add back to top arrow, start on movies and register.html

---
+++ script.js
@@ -0,0 +1,20 @@
+const backToTop = document.getElementById('back-to-top');
+const movies = document.getElementById('movies-area');
+
+/**
+ * When user scrolls down the arrow will show up
+ */
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 320) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+});
+
+/**
+ * When user clicks on the arrow icon it will take him back to top
+ */
+backToTop.addEventListener('click', () => {
+    window.scrollTo({top:0});
+})
